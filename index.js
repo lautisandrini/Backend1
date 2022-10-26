ed49945e56267789967e7552d20f521211224cd0
Lauti Sandrini <sandrini.it@gmail.com>
2022-10-26 19:50:42 -0300

    termino desafio

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () =>
+console.log(`Server running port ${PORT}`)
+);
+server.on("Error", (err) => console.log(`Error: ${err}`));
+
+const Container = require("./container.js");
+const products = new Container("products.txt");
+
+app.get("/", (req, res) => {
+  res.send("<h1>Server with Express</h1>");
+});
+
+app.get("/products", async (req, res) => {
+  const showAll = await products.getAll();
+  res.send({ "All products": showAll });
+});
+
+app.get("/productRandom", async (req, res) => {
+  const prods = await products.getAll();
+  const productRandomIndex = Math.floor(Math.random() * prods.length);
+  res.send({ "Products Random": prods[productRandomIndex] });
+});
